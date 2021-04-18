dfb16ac8c5993c9de853f388d4771c822f09353f
Nick Hennessy <njhnny@gmail.com>
2021-04-18 11:00:50 -0700

    pass second test
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,9 +10,23 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
+
+    [TestMethod]
+public void GetDescription_ReturnsDescription_String()
+{
+  //Arrange
+  string description = "Walk the dog.";
+  Item newItem = new Item(description);
+
+  //Act
+  string result = newItem.Description;
+
+  //Assert
+  Assert.AreEqual(description, result);
+}
 
   }
 }
