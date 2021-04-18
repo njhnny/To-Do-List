fc5da8e84296ee2ec36249a7236705bc37a5e6a6
Nick Hennessy <njhnny@gmail.com>
2021-04-18 10:18:32 -0700

    add csproj boilerplate
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using ToDoList.Models;
+using System;
 
 namespace ToDoList.Tests
 {
@@ -10,9 +12,26 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test"); //We pass in "test" as an argument here.
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
 
   }
 }
